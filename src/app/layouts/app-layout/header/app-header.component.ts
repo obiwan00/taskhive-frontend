4137d9d8e71c97e12dd47237680e2f81081a11c1
Ivan Maidaniuk <ivan.maidaniuk@gmail.com>
2025-12-23 14:25:14 +0200

    feat: enhance an app-header with user info and logo components

--- src/app/layouts/app-layout/header/app-header.component.ts
+++ src/app/layouts/app-layout/header/app-header.component.ts
@@ -1,21 +1,32 @@
 import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 import { AuthService } from '@core/auth';
+import { UserService } from '@core/user';
+import { LogoComponent, UserInfoComponent } from '@shared/ui';
 
 @Component({
   selector: 'app-app-header',
   imports: [
     RouterLink,
     RouterLinkActive,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
+    LogoComponent,
+    UserInfoComponent
   ],
   templateUrl: './app-header.component.html',
   styleUrl: './app-header.component.scss',
 })
 export class AppHeaderComponent {
   private readonly authService = inject(AuthService);
+  private readonly userService = inject(UserService);
+
+  protected readonly userProfile = this.userService.profile;
 
   protected logout(): void {
     this.authService.logout();
--- src/app/layouts/auth-layout/auth-layout.component.ts
+++ src/app/layouts/auth-layout/auth-layout.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { LogoComponent } from '@shared/ui';
+
 @Component({
   selector: 'app-auth-layout',
-  standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LogoComponent],
   templateUrl: './auth-layout.component.html',
   styleUrl: './auth-layout.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
